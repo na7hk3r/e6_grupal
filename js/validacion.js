5b5ff122f9ed9a8347ebdcf3aa1f7c895514da13
na7hk3r <104522465+na7hk3r@users.noreply.github.com>
2022-10-27 15:12:26 -0300

    Falta validacion externa

--- js/validacion.js
+++ js/validacion.js
@@ -13,11 +13,6 @@ password2.addEventListener("input", validarContraseña);
   }};
 
 
-
-
-
-
-
 //Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
 function validación() {
     'use strict'
@@ -38,42 +33,3 @@ function validación() {
         }, false)
       })
   };
-
-/*
-
-/////// Funcion extraída de Documentacion de Bootstrap ///////
-
-// Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
-(function () {
-  "use strict";
-
-  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-  var forms = document.querySelectorAll(".needs-validation");
-
-  // Bucle sobre ellos y evitar el envío
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-
-        form.classList.add("was-validated");
-      },
-      false
-    );
-  });
-})();
-
-// function validarContraseñas() {
-//   if (password1 === password2 && password1 == '.isvalid') {
-//     password1.classList.add('was-validated')
-//   }
-// }
-
-// document.getElementById('submit').addEventListener('click', () => {
-//   validarContraseñas();
-// })
-*/
